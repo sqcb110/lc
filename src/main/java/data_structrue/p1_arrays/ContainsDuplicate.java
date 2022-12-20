e929ee717e7e173403321df84bd257a66bbb8031
sqcb110 <312210751@qq.com>
2022-12-20 18:14:08 +0800

    2

--- src/main/java/data_structrue/p1_arrays/ContainsDuplicate.java
+++ src/main/java/data_structrue/p1_arrays/ContainsDuplicate.java
@@ -3,15 +3,30 @@ package data_structrue.p1_arrays;
 //217. 存在重复元素
 //给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class ContainsDuplicate {
-    public boolean containsDuplicate(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 4;
+        nums[4] = 4;
+        System.out.println(containsDuplicate(nums));
+    }
+    public static boolean containsDuplicate(int[] nums) {
         HashSet set = new HashSet();
-        
+        for (int num:nums
+             ) {
+            set.add(num);
+        }
+        int a = nums.length;
+        int b = set.size();
+        if (a==b){
+            return false;
+        }
         return true;
     }
 }
