a0ecd7b92b6acf99176bf91979f2a80b4570d842
yanchangbo <312210751@qq.com>
2023-12-12 21:50:36 +0800

    2023-12-12-21

--- src/main/java/data_structrue/p1_arrays/Test7_Clear_Matrix_Advanced.java
+++ src/main/java/data_structrue/p1_arrays/Test7_Clear_Matrix_Advanced.java
@@ -1,10 +1,8 @@
 package data_structrue.p1_arrays;
 
-//编写一种算法，若M × N矩阵中某个元素为0，则将其所在的行与列清零。
-
 import java.util.HashSet;
 
-public class Test7_Clear_Matrix {
+public class Test7_Clear_Matrix_Advanced {
     public static void main(String[] args) {
         int[][] mat = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
         setZeroes(mat);
@@ -17,34 +15,24 @@ public class Test7_Clear_Matrix {
     }
 
     public static void setZeroes(int[][] matrix) {
-        HashSet<Integer> rows = new HashSet<Integer>();
-        HashSet<Integer> lines = new HashSet<Integer>();
+        boolean[] rows = new boolean[matrix.length];
+        boolean[] lines = new boolean[matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
-                    rows.add(i);
-                    lines.add(j);
+                    rows[i] = true;
+                    lines[j] = true;
                 }
             }
         }
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if (rows.contains(i) | lines.contains(j)) {
+                if (rows[i] | lines[j]) {
                     matrix[i][j] = 0;
                 }
             }
         }
-/*        for (Integer itg: rows) {
-            for (int i = 0; i < matrix[itg.intValue()].length; i++) {
-                matrix[itg.intValue()][i] = 0;
-            }
-        }
 
-        for (Integer itg: lines) {
-            for (int j = 0; j < matrix.length; j++) {
-                matrix[j][itg.intValue()] = 0;
-            }
-        }*/
     }
 }
