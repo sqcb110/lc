9361f1bf94f734dd90b2d1a08cf609295de8e739
jiashuqi <312210751@qq.com>
2023-12-15 01:15:31 +0800

    2023-12-15-01

--- src/main/java/data_structrue/p1_arrays/Test8_Matrix_Diagonal_Ergodic_revised.java
+++ src/main/java/data_structrue/p1_arrays/Test8_Matrix_Diagonal_Ergodic_revised.java
@@ -15,30 +15,52 @@ public class Test8_Matrix_Diagonal_Ergodic_revised {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
+        System.out.println();
+        int[][] mat1 = {{1, 2, 3}, {4, 5, 6}};
+        int[] arr1 = diagonalErgodic(mat1);
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+        }
+        System.out.println();
+        int[][] mat2 = {{1, 2}, {4, 5}, {7, 8}};
+        int[] arr2 = diagonalErgodic(mat2);
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+        }
     }
 
     public static int[] diagonalErgodic(int[][] mat) {
         int index = 0;
         int n = mat.length;
         int m = mat[0].length;
-        int mvn = Math.max(m, n);
+        int mvnb = Math.max(m, n);
+        int mvns = Math.min(m, n);
         int[] arr = new int[m * n];
         for (int i = 0; i < m + n - 1; i++) {
-            for (int j = 0; j < i + 1 & j < m + n - i - 1 & j < mvn; j++) {
-                if (i == 0 & j == 0) {
-                    arr[index] = mat[i][j];
-                    index++;
-                } else if (i < mvn & i % 2 == 1) {
+            for (int j = 0; j < i + 1 & j < m + n - i - 1 & j < mvns; j++) {
+                if (i < mvns & i % 2 == 1) { //开始阶段
                     arr[index] = mat[j][i - j];
                     index++;
-                } else if (i < mvn & i % 2 == 0) {
+                } else if (i < mvns & i % 2 == 0) { //开始阶段
                     arr[index] = mat[i - j][j];
                     index++;
-                } else if (i >= mvn & i % 2 == 1) {
-                    arr[index] = mat[j + 1 + i - n][n - j - 1];
+                } else if (i >= mvns & i < mvnb & m >= n & i % 2 == 1) { //中间阶段1
+                    arr[index] = mat[j][i - j];
                     index++;
-                } else {
-                    arr[index] = mat[n - j - 1][j + 1 + i - n];
+                } else if (i >= mvns & i < mvnb & m >= n & i % 2 == 0) { //中间阶段1
+                    arr[index] = mat[n - 1 - j][i - n + 1 + j];
+                    index++;
+                } else if (i >= mvns & i < mvnb & m <= n & i % 2 == 1) { //中间阶段2
+                    arr[index] = mat[i - mvns + 1 + j][mvns - 1 - j];
+                    index++;
+                } else if (i >= mvns & i < mvnb & m <= n & i % 2 == 0) { //中间阶段2
+                    arr[index] = mat[i - j][j];
+                    index++;
+                } else if (i >= mvnb & i % 2 == 1) { //结尾阶段
+                    arr[index] = mat[i - m + 1 + j][m - 1 - j];
+                    index++;
+                } else if (i >= mvnb & i % 2 == 0) { //结尾阶段
+                    arr[index] = mat[n - 1 - j][i - n + 1 + j];
                     index++;
                 }
             }
