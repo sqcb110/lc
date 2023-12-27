8e56b7ec7fbb7f1782270fe768ab6e03a0cadd98
jiashuqi <312210751@qq.com>
2023-12-28 00:21:34 +0800

    2023-12-28-00

--- src/main/java/data_structrue/p1_arrays/Test10_Longest_Palindrome.java
+++ src/main/java/data_structrue/p1_arrays/Test10_Longest_Palindrome.java
@@ -8,16 +8,47 @@ package data_structrue.p1_arrays;
 
 public class Test10_Longest_Palindrome {
     public static void main(String[] args) {
-
+        String s = "babad";
+        String subs = longestPalindrome(s);
+        System.out.println(subs);
     }
 
     public static String longestPalindrome(String str){
         //整型数组记录开始、结尾、长度
-        int[] pal = new int[3];
+        int[] pal = {0,0,0};
         for (int i = 1; i < pal.length - 1; i++) {
-            
+            boolean dan = true;
+            boolean shuang = true;
+            int bigin_dan = i;
+            int end_dan = i;
+            int bigin_shuang = i + 1;
+            int end_shuang = i;
+            //长度为奇数的回文子串：
+            while(dan){
+                bigin_dan -= 1;
+                end_dan += 1;
+                if(bigin_dan >=0 && end_dan <= str.length() - 1  && str.charAt(bigin_dan)==str.charAt(end_dan) && end_dan + 1 - bigin_dan > pal[2]){
+                    pal[0] = bigin_dan;
+                    pal[1] = end_dan;
+                    pal[2] = end_dan + 1 -bigin_dan;
+                }else{
+                    dan = false;
+                }
+            }
+            //长度为偶数的回文子串：
+            while(shuang){
+                bigin_shuang -= 1;
+                end_shuang += 1;
+                if(bigin_shuang >=0 && end_shuang <= str.length() - 1  && str.charAt(bigin_shuang)==str.charAt(end_shuang) && end_dan + 1 - bigin_dan > pal[2]){
+                    pal[0] = bigin_shuang;
+                    pal[1] = end_shuang;
+                    pal[2] = end_shuang + 1 -bigin_shuang;
+                }else{
+                    shuang = false;
+                }
+            }
         }
 
-        return null;
+        return str.substring(pal[0],pal[1]+1);
     }
 }
